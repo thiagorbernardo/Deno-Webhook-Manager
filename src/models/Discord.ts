c99a8c081f6eab872a609d975080cc97cf9ef8c6
Thiago Ramos <thiagorbernardo@hotmail.com>
2021-06-17 09:51:30 -0300

    feat: discord server models

---
+++ src/models/Discord.ts
@@ -0,0 +1,41 @@
+
+
+export interface DiscordMessage {
+    username?:   string;
+    "avatar_url"?: string;
+    content:    string;
+    embeds?:     Embed[];
+}
+
+export interface Embed {
+    author:      Author;
+    title:       string;
+    url:         string;
+    description: string;
+    color:       number;
+    fields:      Field[];
+    thumbnail:   Image;
+    image:       Image;
+    footer:      Footer;
+}
+
+export interface Author {
+    name:     string;
+    url:      string;
+    "icon_url": string;
+}
+
+export interface Field {
+    name:    string;
+    value:   string;
+    inline?: boolean;
+}
+
+export interface Footer {
+    text:     string;
+    "icon_url": string;
+}
+
+export interface Image {
+    url: string;
+}
